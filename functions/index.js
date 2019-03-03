7f1a224f258641865ffb41a6c0d11bcacc188898
nameless <gfedcba625@gmail.com>
2019-03-04 00:45:59 +0200

    added trigger for VOIP calling

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,66 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const _ = require('lodash');
 admin.initializeApp();
+
+const VOICE_CALLING_TAG = 'VOICE_CALLING_TAG';
+const NOTIFICATION_TAG = 'NOTIFICATION_TAG';
+
+exports.voipNotifier = functions.database.ref('/Users/{userId}/voiceCall').onWrite(async (change, context) => {
+
+    // State of the user which initiator of call else will be port for connection (for java server)
+    const CALLING_STATE = "calling...";
+
+    if (!change.after.exists() || change.after.val() === CALLING_STATE) {
+        return null;
+    }
+
+    const notificationPayload = {
+        data: {
+            privateRoomPort: change.after.val(),
+            tag: VOICE_CALLING_TAG
+        }
+    };
+    const registrationId = await change.after.ref.parent.once("value")
+        .then(res => res.val().registrationTokenId);
+    return admin.messaging().sendToDevice(registrationId, notificationPayload);
+
+});
 
 exports.notificationSender = functions.database.ref('/Dialogs/{dialogId}/lastMessage').onWrite(async (change, context) => {
     if (!change.after.exists()) {
         return null;
     }
+
+    const prevSnapshot = {
+        ...change.before.val(),
+        read: null
+    };
+    const currentSnapshot = {
+        ...change.after.val(),
+        read: null
+    };
+
+    if (_.isEqual(prevSnapshot, currentSnapshot)) {
+        return null;
+    }
+
     console.log(change.before.val());
     console.log(change.after.val());
     console.log('__');
 
-    const dialogSnapshot = await change.after.ref.parent.once("value");
+    const dialogSnapshot = await change.after.ref.parent.once("value")
+        .then(res => res.val());
     const lastMessage = change.after.val();
     const payload = {
         data: {
             content: lastMessage.fileType || lastMessage.content,
+            tag: NOTIFICATION_TAG
         }
     };
     const fetchSpeakersRegIds = [];
 
-    for (let speaker in dialogSnapshot.val().speakers) {
+    for (let speaker in dialogSnapshot.speakers) {
         fetchSpeakersRegIds.push(
             admin.database()
                 .ref('Users')
